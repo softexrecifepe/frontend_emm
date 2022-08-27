7db308c1054759abd7bdacdf259d61a731960f97
Claudio128 <cllaudio128@gmail.com>
2022-08-27 07:15:38 -0300

    Atualização do site (Utilizando JavaScript)

---
+++ script/script.js
@@ -0,0 +1,38 @@
+/*function eduardo(){
+    document.getElementById('title').innerHTML="Temperatura - Sr. Eduardo - (Boa Viagem) Recife/PE";
+    document.getElementById('id').src="https://thingspeak.com/channels/1631164/charts/1?bgcolor=%23F8F8FF&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
+    setTimeout("claudio()", 5000)
+}
+
+function claudio(){
+    document.getElementById('title').innerHTML="Temperatura - Claudio Andrade - (Boa Viagem) Recife/PE";
+    document.getElementById('id').src="https://thingspeak.com/channels/1598550/charts/1?bgcolor=%23F8F8FF&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
+    setTimeout("esdras()", 5000)
+}
+
+function esdras(){
+    document.getElementById('title').innerHTML="Temperatura - Êsdras Jhonnatas - (Boa Viagem) Recife/PE";
+    document.getElementById('id').src="https://thingspeak.com/channels/1629491/charts/1?bgcolor=%23F8F8FF&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
+    setTimeout("eduardo()", 5000)
+}
+*/
+function eduardo(){
+    document.getElementById('title').innerHTML="Temperatura - Sr. Eduardo - (Boa Viagem) Recife/PE";
+    document.getElementById('id').src="https://thingspeak.com/channels/1631164/charts/1?bgcolor=%23F8F8FF&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
+    setTimeout("claudio()", 3000)
+    document.getElementById('aId').href="https://estacaomm.w3spaces.com/eduardo.html"
+    }
+    
+function claudio(){
+    document.getElementById('title').innerHTML="Temperatura - Claudio Andrade - (Boa Viagem) Recife/PE";
+    document.getElementById('id').src="https://thingspeak.com/channels/1598550/charts/1?bgcolor=%23F8F8FF&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
+    setTimeout("esdras()", 3000)
+    document.getElementById('aId').href="https://estacaomm.w3spaces.com/claudio.html"
+    }
+    
+function esdras(){
+    document.getElementById('title').innerHTML="Temperatura - Êsdras Jhonnatas - (Boa Viagem) Recife/PE";
+    document.getElementById('id').src="https://thingspeak.com/channels/1629491/charts/1?bgcolor=%23F8F8FF&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
+    setTimeout("eduardo()", 3000)
+    document.getElementById('aId').href="https://estacaomm.w3spaces.com/esdras.html"
+    }
