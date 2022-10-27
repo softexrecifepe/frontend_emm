8e575c095690e5133ba3a769805cba2491cbf2d1
Claudio128 <cllaudio128@gmail.com>
2022-10-27 20:44:52 -0300

    Atualização

--- Bootstrap/script.js
+++ Bootstrap/script.js
@@ -1,13 +1,14 @@
-document.addEventListener("scroll", function() {
-  var posicaoy = window.pageYOffset;
-  if (posicaoy <= 100) {
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > 100) {
+    document.getElementById("navbar").style.top = "0";
     const nav = document.getElementById("main-header");
-    nav.classList.remove("fixed-top");
+    nav.classList.remove("hidden")
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+    const nav = document.getElementById("main-header");
     nav.classList.add("hidden");
   }
-  else{
-    const nav = document.getElementById("main-header");
-    nav.classList.add("fixed-top");
-    nav.classList.remove("hidden")
-  }
-});
+  prevScrollpos = currentScrollPos;
+}
