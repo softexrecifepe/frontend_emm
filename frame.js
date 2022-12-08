98c7156f74a22fb634b722c9c32f97e471545e9b
Claudio128 <cllaudio128@gmail.com>
2022-12-08 12:25:58 -0300

    Responsividade da página index.html onde está localizado o Iframe

--- frame.js
+++ frame.js
@@ -1,32 +1,32 @@
 function fablab(){
-    document.getElementById('title').innerHTML="Temperatura - ABA Fab Lab - Recife/PE";
+    document.getElementById('title').innerHTML="ABA Fab Lab - Recife/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1638973/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
     setTimeout("afogados()", 3000)
 }
 
 function afogados(){
-    document.getElementById('title').innerHTML="Temperatura - Afogados da Ingazeira/PE";
+    document.getElementById('title').innerHTML="Afogados da Ingazeira/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1638963/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
     setTimeout("camponesa()", 3000)
 }
 
 /*
 function anderson(){
-    document.getElementById('title').innerHTML="Temperatura - Anderson Gusmão - (Céu Azul) Camaragibe/PE";
+    document.getElementById('title').innerHTML="Anderson Gusmão - (Céu Azul) Camaragibe/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1437739/charts/2?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&timescale=10&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("afogados()", 3000)
     }
 */
 
 function camponesa(){
-    document.getElementById('title').innerHTML="Temperatura - Escola Camponesa - Lagoa Seca/PB";
+    document.getElementById('title').innerHTML="Escola Camponesa - Lagoa Seca/PB";
     document.getElementById('id').src="https://thingspeak.com/channels/1686704/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("marcos()", 3000)
 }
 
 /*
 function caruaru(){
-    document.getElementById('title').innerHTML="Temperatura - Marina Santos - Caruaru/PE";
+    document.getElementById('title').innerHTML="Marina Santos - Caruaru/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1638976/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
     setTimeout("fablab()", 3000)
     document.getElementById('aId').href="https://estacaomm.w3spaces.com/caruaru.html"
@@ -35,7 +35,7 @@ function caruaru(){
 
 /*
 function claudio(){
-    document.getElementById('title').innerHTML="Temperatura - Claudio Andrade - (Boa Viagem) Recife/PE";
+    document.getElementById('title').innerHTML="Claudio Andrade - (Boa Viagem) Recife/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1839053/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
     setTimeout("esdras()", 3000)
 }
@@ -43,7 +43,7 @@ function claudio(){
 
 /*
 function clecio(){
-    document.getElementById('title').innerHTML="Temperatura - Clécio Lima - (Coelhos) Recife/PE";
+    document.getElementById('title').innerHTML="Clécio Lima - (Coelhos) Recife/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1629867/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("marina()", 3000)
     }
@@ -51,7 +51,7 @@ function clecio(){
 
 /*
 function eduardo(){
-    document.getElementById('title').innerHTML="Temperatura - Sr. Eduardo - (Boa Viagem) Recife/PE";
+    document.getElementById('title').innerHTML="Sr. Eduardo - (Boa Viagem) Recife/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1631164/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("claudio()", 3000)
     }
@@ -59,7 +59,7 @@ function eduardo(){
 
 /*
 function esdras(){
-    document.getElementById('title').innerHTML="Temperatura - Êsdras Jhonnatas - (Boa Viagem) Recife/PE";
+    document.getElementById('title').innerHTML="Êsdras Jhonnatas - (Boa Viagem) Recife/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1629491/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("marina()", 3000)
 }
@@ -67,34 +67,34 @@ function esdras(){
 
 /*
 function lucivanio(){
-    document.getElementById('title').innerHTML="Temperatura - Prof Lucivanio - (Janga) Paulista/PE";
+    document.getElementById('title').innerHTML="Prof Lucivanio - (Janga) Paulista/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1598674/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("marcos()", 3000)
     }
 */
 
 function marcos(){
-    document.getElementById('title').innerHTML="Temperatura - Marcos Egito - (Janga) Paulista/PE";
+    document.getElementById('title').innerHTML="Marcos Egito - (Janga) Paulista/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1418948/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&timescale=10&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("mirandiba()", 3000)
 }
 
 /*
 function marina(){
-    document.getElementById('title').innerHTML="Temperatura - Marina Santos - (San Martin) Recife/PE";
+    document.getElementById('title').innerHTML="Marina Santos - (San Martin) Recife/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1631161/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("marcos()", 3000)
 }
 */
 
 function mirandiba(){
-    document.getElementById('title').innerHTML="Temperatura - Mirandiba/PE";
+    document.getElementById('title').innerHTML="Mirandiba/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1638970/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
     setTimeout("fablab()", 3000)
 }
 /*
 function sitioalagoinha(){
-    document.getElementById('title').innerHTML="Temperatura - Sítio Alagoinha - (Distrito Rajada) Petrolina/PE";
+    document.getElementById('title').innerHTML="Sítio Alagoinha - (Distrito Rajada) Petrolina/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1598675/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("camponesa()", 3000)
 }
@@ -102,7 +102,7 @@ function sitioalagoinha(){
 
 /*
 function sitiocruz(){
-    document.getElementById('title').innerHTML="Temperatura - Sítio Cruz Nova - Garanhuns/PE";
+    document.getElementById('title').innerHTML="Sítio Cruz Nova - Garanhuns/PE";
     document.getElementById('id').src="https://thingspeak.com/channels/1598548/charts/1?bgcolor=%23ffffff&color=%23d62020&days=30&dynamic=true&results=100&title=Temperatura+-+%C2%BAC&type=spline&xaxis=Tempo+Decorrido+%2F+Hora&yaxis=Graus+Celsios++-+%C2%BAC";
     setTimeout("sitioalagoinha()", 3000)
     }
